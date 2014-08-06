03199182b3afa96a589602ea1ae41ca3661c03a2
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-08-06 23:15:15 +0900

    Remove comments

--- source/js/main.js
+++ source/js/main.js
@@ -1,20 +1,4 @@
 //= require jquery
-
-// Load all Bootstrap JavaScripts
 //= require bootstrap
-
-// Load individual modules
-// require bootstrap/affix
-// require bootstrap/alert
-// require bootstrap/button
-// require bootstrap/carousel
-// require bootstrap/collapse
-// require bootstrap/dropdown
-// require bootstrap/tab
-// require bootstrap/transition
-// require bootstrap/scrollspy
-// require bootstrap/modal
-// require bootstrap/tooltip
-// require bootstrap/popover
 
 //= require_tree .
